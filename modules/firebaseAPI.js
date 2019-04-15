2a3f88af5a6c0ecc444f153d65056a6bdc977f14
Eymric <32844398+Eymric@users.noreply.github.com>
2019-04-15 12:12:21 +0200

    Add files via upload

--- modules/firebaseAPI.js
+++ modules/firebaseAPI.js
@@ -9,8 +9,6 @@ export const createUser = (email, password, username) => {
 	});
 	})
 	.catch((error) => console.log('createUser error:', error));
-	
-	
 }
 
 export const logout = () => {
@@ -20,4 +18,28 @@ export const logout = () => {
 export const connexion = (email, password) => {
 	firebase.auth().signInWithEmailAndPassword(email, password)
 		.catch((error) => console.log('erreur connexion: ', error));
+}
+
+export const trajet = () => {
+	firebase.database().ref('trajet/' + '002' ).set({
+		nb_meubles: 4,
+		poids: 65,
+		estimation_prix: 30
+	  });
+
+	  firebase.database().ref('adresse/' + '001' ).set({
+		lieu: '12 rue des roses',
+		cp: 75018,
+		ville: 'Paris',
+		
+	  });
+
+	  firebase.database().ref('adresse/' + '002' ).set({
+		lieu: '17 Sente des Hauts Jardins',
+		cp: 60440,
+		ville: 'Nanteuil-le-Haudouin',
+		
+	  });
+
+
 }
