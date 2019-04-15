4da95f54c675823f9027789359e01651aa25ae3e
levy75 <levy.zire@gmail.com>
2019-04-15 11:50:38 +0200

    Create Page AddTrajetScreen

---
+++ screens/AddTrajetScreen.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { AppRegistry, Image, Text, View, StyleSheet, TextInput, Dimensions, Button } from 'react-native';
+import firebase from 'firebase';
+import * as firebaseAPI from '../modules/firebaseAPI';
+
+export default class AddTrajetScreen extends Component{
+
+    static navigationOptions = {
+        title : 'Login',
+      };
+      
+    render(){
+        return(
+            <View>
+                
+            </View> 
+        )
+    }
+
+}
